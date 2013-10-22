9580bf7bbe87aab1afbf730d8c5f76fba0d34a1c
Philip Browning <philipjbrowning@gmail.com>
2013-10-22 12:19:23 -0400

    Initial Commit

---
+++ SYCS 659 - Keccak/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SYCS 659 - Keccak
+//
+//  Created by Philip J Browning II on 10/22/13.
+//  Copyright (c) 2013 Philip Browning. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
